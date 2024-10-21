c5c51fc6a98fe076027f6a91d59e53570ef956f3
Ron <rongoods@gmail.com>
2024-10-21 16:20:59 +0200

    serialize function added and refactored

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -10,28 +10,32 @@ def load_data(file_path):
     with open(file_path, "r") as file:
         return json.load(file)
 
-animals_data = load_data('animals_data.json')
+def serialize_animal(animal_obj):
+    output = ''
+    output += '<li class="cards__item">\n'
 
-output = ''
-for animal in animals_data:
-    output += '<li class="cards__item">'
-    if 'name' in animal:
-        name = animal['name']
-        output += f'<div class="card__title">{name}<br/>\n</div>'
-    if 'characteristics' in animal and 'diet' in animal['characteristics']:
-        diet = animal['characteristics']['diet']
-        output += f'<p class="card__text"><strong>Diet:</strong> {diet}<br/></p>'
-    if 'locations' in animal:
-        locations = animal['locations']
-        output += f'<p class="card__text"><strong>First Location:</strong> {locations[0]}<br/></p>'
-    if 'characteristics' in animal and 'type' in animal['characteristics']:
-        type_of_animal = animal['characteristics']['type']
-        output += f'<p class="card__text"><strong>Type:</strong> {type_of_animal}<br/></p>'
+    if 'name' in animal_obj:
+        output += f'<div class="card__title">{animal_obj["name"]}</div>\n'
+    if 'characteristics' in animal_obj and 'diet' in animal_obj['characteristics']:
+        output += f'<p class="card__text"><strong>Diet:</strong> {animal_obj["diet"]}</p>\n'
+    if 'locations' in animal_obj:
+        locations = animal_obj['locations']
+        output += f'<p class="card__text"><strong>First Location:</strong> {animal_obj["locations[0]"]}</p>\n'
+    if 'characteristics' in animal_obj and 'type' in animal_obj['characteristics']:
+        output += f'<p class="card__text"><strong>Type:</strong> {animal_obj["type_of_animal"]}</p>\n'
     output += '</li>'
+    print(output)
 
-print(output)
+def main():
+    animals_data = load_data('animals_data.json')
+    output = ''
+    for animal in animals_data:
+        output += serialize_animal(animal)
 
-finished_html = html_contents.replace("__REPLACE_ANIMALS_INFO__", output)
+    finished_html = html_contents.replace("__REPLACE_ANIMALS_INFO__", output)
 
-with open('animals.html', "w") as file:
-    file.write(finished_html)
+    with open('animals.html', "w") as file:
+        file.write(finished_html)
+
+if __name__ == "__main__":
+    main()
