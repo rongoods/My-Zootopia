35032e4b826ce09c7e987c2febb442d89f6a087d
Ron <rongoods@gmail.com>
2024-10-21 15:50:15 +0200

    Item Serialization complete

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -14,19 +14,22 @@ animals_data = load_data('animals_data.json')
 
 output = ''
 for animal in animals_data:
+    output += '<li class="cards__item">'
     if 'name' in animal:
         name = animal['name']
-        output += f"Name: {name}\n"
+        output += f"Name: {name}<br/>\n"
     if 'characteristics' in animal and 'diet' in animal['characteristics']:
         diet = animal['characteristics']['diet']
-        output += f"Diet: {diet}\n"
+        output += f"Diet: {diet}<br/>\n"
     if 'locations' in animal:
         locations = animal['locations']
-        output += f"First Location: {locations[0]}\n"
+        output += f"First Location: {locations[0]}<br/>\n"
     if 'characteristics' in animal and 'type' in animal['characteristics']:
         type_of_animal = animal['characteristics']['type']
-        output += f"Type: {type_of_animal}\n"
+        output += f"Type: {type_of_animal}<br/>\n"
+    output += '</li>'
     output += "\n"
+print(output)
 
 finished_html = html_contents.replace("__REPLACE_ANIMALS_INFO__", output)
 
