7200934b5d316494d04bda1805dddd8ee4a63eb6
Ron <rongoods@gmail.com>
2025-05-07 16:55:50 +0200

    animals api and search enabled

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,78 +1,3 @@
-
-# import json
-# import requests
-#
-#
-# def read_html(template_path):
-#     """Read an HTML template file and return its contents."""
-#     with open(template_path, "r", encoding="utf-8") as file:
-#         return file.read()
-#
-#
-# def fetch_animal_data(animal_name):
-#     """Fetch animal data from the API based on a search term."""
-#     url = f"https://api.api-ninjas.com/v1/animals?search={animal_name}"
-#     response = requests.get(url)
-#     response.raise_for_status()
-#     return response.json()
-#
-#
-# def serialize_animal(animal_obj):
-#     """Generate HTML for a single animal entry."""
-#     output = '<li class="cards__item">\n'
-#
-#     if "name" in animal_obj:
-#         output += f'<div class="card__title">{animal_obj["name"]}</div>\n'
-#
-#     characteristics = animal_obj.get("characteristics", {})
-#
-#     diet = characteristics.get("diet")
-#     if diet:
-#         output += f'<p class="card__text"><strong>Diet:</strong> {diet}</p>\n'
-#
-#     locations = animal_obj.get("locations", [])
-#     if locations:
-#         output += f'<p class="card__text"><strong>First Location:</strong> {locations[0]}</p>\n'
-#
-#     animal_type = characteristics.get("type")
-#     if animal_type:
-#         output += f'<p class="card__text"><strong>Type:</strong> {animal_type}</p>\n'
-#
-#     output += '</li>\n'
-#     return output
-#
-#
-# def generate_html(animals_data, html_template):
-#     """Generate the final HTML content by inserting animal data."""
-#     output = ''.join(serialize_animal(animal) for animal in animals_data)
-#     return html_template.replace("__REPLACE_ANIMALS_INFO__", output)
-#
-#
-# def main():
-#     """Main function to fetch data, generate HTML, and write to a file."""
-#     html_template = read_html('animals_template.html')
-#     animals_data = fetch_animal_data("Fox")
-#
-#     finished_html = generate_html(animals_data, html_template)
-#
-#     with open('animals.html', "w", encoding="utf-8") as file:
-#         file.write(finished_html)
-#
-#
-# if __name__ == "__main__":
-#     main()
-#
-
-
-# import json
-# import requests
-#
-#
-# def read_html(template_path):
-#     """Read an HTML template file and return its contents."""
-#     with open(template_path, "r", encoding="utf-8") as file:
-#         return file.read()
-
 import requests
 
 API_KEY = "aTap3/8ErjpLphigqe6Fcw==deLuV38G8sgyJ5ug"
@@ -81,7 +6,7 @@ API_KEY = "aTap3/8ErjpLphigqe6Fcw==deLuV38G8sgyJ5ug"
 def fetch_animal_data(animal_name):
     url = "https://api.api-ninjas.com/v1/animals"
     headers = {
-        "X-Api-Key": API_KEY
+        "X-Api-Key": aTap3/8ErjpLphigqe6Fcw==deLuV38G8sgyJ5ug
     }
     params = {
         "name": animal_name
